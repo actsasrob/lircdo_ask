79b2417bb14b36f4fcb16703ceb1ecfc56d9008e
actsasrob <acts.as.rob@gmail.com>
2018-11-10 10:40:11 -0500

    Updates for node v8.10. Now using ask cli to deploy ask skill and lambda function

---
+++ lambda/custom/index.js
@@ -0,0 +1,63 @@
+// Big thanks to Oscar Merry and the excellent Advanced Alexa course over at A Cloud Guru
+// Source code for this project borrowed from: https://github.com/MerryOscar/voice-devs-lessons
+
+//var Alexa = require('alexa-sdkr'); // v1 sdk
+var Alexa = require('ask-sdk-v1adapter'); // v2 sdk
+//var Alexa = require('alexa-sdk-core'); // v2 sdk no adapter
+
+// Constants
+var constants = require('constants/constants');
+
+// Handlers
+var initialStateHandlers = require('handlers/initialStateHandlers');
+var pairingStateHandlers = require('handlers/pairingStateHandlers');
+var mainStateHandlers = require('handlers/mainStateHandlers');
+
+exports.handler = function(event, context, callback){
+  var alexa = Alexa.handler(event, context);
+
+  alexa.appId = constants.appId;
+  alexa.dynamoDBTableName = constants.dynamoDBTableName;
+
+  alexa.registerHandlers(
+    initialStateHandlers,
+    pairingStateHandlers,
+    mainStateHandlers
+  );
+
+  alexa.execute();
+};
+
+//let skill;
+// 
+//exports.handler = function (event, context) {
+//     // Prints Alexa Event Request to CloudWatch logs for easier debugging
+//     console.log(`===EVENT===${JSON.stringify(event)}`);
+//     if (!skill) {
+//     //skill.appID = constants.appId;
+//     skill = Alexa.SkillBuilders.custom()
+//     .addRequestHandlers(
+//             initialStateHandlers,
+//             pairingStateHandlers,
+//             mainStateHandlers
+//         )
+//	 .withTableName(constants.dynamoDBTableName)
+//	 .withAutoCreateTable(true)
+//         .create();
+//     }
+//
+//     return skill.invoke(event,context);
+// }
+
+//exports.handler = Alexa.SkillBuilders.standard()
+//  .addRequestHandlers(
+//  initialStateHandlers,
+//  pairingStateHandlers,
+//  mainStateHandlers
+//  )
+//  //.addRequestInterceptors(RequestLog)
+//  //.addResponseInterceptors(ResponseLog)
+//  //.addErrorHandlers(ErrorHandler)
+//  .withTableName('constants.dynamoDBTableName')
+//  .withAutoCreateTable(true)
+//.lambda();
--- publish_lambda.sh
+++ publish_lambda.sh
@@ -2,11 +2,13 @@
 
 nvm use 8.0
 
+cwd=$(pwd)
+
 overallstatus=0
 which node > /dev/null 2>&1
 if [ "$?" -eq 0 ]; then
    echo "Running node --check..."
-   cd lambda
+   cd lambda/custom
    node --check index.js
    status=$?
    if [ "$status" -ne 0 ]; then
@@ -15,7 +17,7 @@ if [ "$?" -eq 0 ]; then
    fi
    listofjsfiles=$(find handlers/ helpers/ constants/ -name "*.js")
    for file in $listofjsfiles; do
-      #echo "checking file $file"
+      echo "checking file $file"
       node --check $file
       status=$?
       if [ "$status" -ne 0 ]; then
@@ -23,7 +25,7 @@ if [ "$?" -eq 0 ]; then
          overallstatus=$status
       fi
    done 
-   cd ..
+   cd $cwd
 fi
 
 if [ "$overallstatus" -ne 0 ]; then
@@ -31,9 +33,11 @@ if [ "$overallstatus" -ne 0 ]; then
    exit $overallstatus
 fi
 
-rm lambda.zip
-cd lambda
+#exit $overallstatus
+
+rm -f lambda.zip
+cd lambda/custom
 npm install
-zip -r --quiet ../lambda.zip *
-cd ..
-aws lambda update-function-code --function-name lircdo_nodejsv08 --zip-file fileb://lambda.zip --profile robhughes_us-east-1
+zip -r --quiet $cwd/lambda.zip * 
+cd $cwd 
+aws lambda update-function-code --function-name ask-custom-lircdo_ask_nodejsv8-default --zip-file fileb://lambda.zip --profile robhughes_us-east-1
