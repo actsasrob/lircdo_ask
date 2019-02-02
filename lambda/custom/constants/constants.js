e3ca70945f80e3f4bd2c2f1c029245906697e0e8
actsasrob <acts.as.rob@gmail.com>
2019-02-02 17:25:21 -0500

    Change invocation name to 'lurk do' to be phonetically closer to 'LIRC do'

--- lambda/custom/constants/constants.js
+++ lambda/custom/constants/constants.js
@@ -1,7 +1,7 @@
 var constants = Object.freeze({
 
   // App-ID. Set The App ID
-  appId : 'amzn1.ask.skill.2ade1cff-3cb6-4230-94b6-4ff20ec3caf7',
+  appId : 'amzn1.ask.skill.1640f539-4c65-43e2-8523-62478f40d2ba',
 
   //  DynamoDB Table Name
   dynamoDBTableName : 'lircdo_table01',
