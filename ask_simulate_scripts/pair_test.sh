90e6d6716c4f3fae6355ed9e4db4f9789fbc879b
actsasrob <acts.as.rob@gmail.com>
2019-03-02 17:14:04 -0500

    Update slot descriptions

--- ask_simulate_scripts/pair_test.sh
+++ ask_simulate_scripts/pair_test.sh
@@ -15,6 +15,7 @@ ask simulate -l en-US -p default -t "open baba zoo"
 sleep 2 
 ask simulate -l en-US -p default -t "my application pin is one seven eight"
 sleep 2
+#ask simulate -l en-US -p default -t "my application address is one zero eight dot five one dot two five dot six seven"
 ask simulate -l en-US -p default -t "my application address is ${ip_text}"
 sleep 2
 ask simulate -l en-US -p default -t "yes"
